25dc14a5dca70a8ef27baa52b27cba6b3dcc3294
AllenXu <xulongzju@163.com>
2014-08-21 23:52:45 +0800

    rm

--- PermulationAndCombination/PermulationAndCombination/main.cpp
+++ PermulationAndCombination/PermulationAndCombination/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
+//排列问题，三种方法， DFS， DP， next_permulation
+//方法一、DFS
 void swapChar(char* p1, char* p2)
 {
 	char tmp = *p1;
@@ -34,10 +38,55 @@ void Permulation(char* str)
 	if (str == NULL || strlen(str) == 0)
 		return;
 
+	sort(str, str + strlen(str));
 	PermulationDFS(str, 0);
 }
 
+//DP，若子串f(i - 1)的排列已知，求f(i)的排列，只需将str[i]往f(i - 1)的所有串的空隙中塞，当然有了递推公式，用递归也是可以实现的
+//下面用递归实现
 
+vector<string> PermulationHelper(char* str, int len) 
+{
+	int total = strlen(str);
+    vector<string> res;
+	if (len <= 0 || len > total || str == NULL)
+	{
+		res.push_back("");
+		return res;
+	}
+
+	vector<string> subRes = PermulationHelper(str, len - 1);
+
+	char cur = str[len - 1];
+	
+	for (int i = 0; i < subRes.size(); ++i)
+	{
+		//往字符串中各位置插入当前字符
+		res.push_back(string(subRes[i] + cur));
+		for (int j = 0; j < subRes[i].size(); j++)
+		{
+			string tmp = subRes[i];
+			res.push_back(tmp.insert(j, string("") + cur));
+		}
+	}
+	return res;
+}
+
+void PermulationRecur(char* str)
+{
+	if (str == NULL)
+		return;
+
+	vector<string> res = PermulationHelper(str, strlen(str));
+	for (int i = 0; i < res.size(); ++i)
+	{
+		if (res[i].size() != 0)
+			cout << res[i] << endl;
+	}
+}
+
+//组合问题，实质就是求一个集合的所有子集，有三种方法，DFS, DP, 基于选取与不选取的位操作方法
+//方法一
 void CombinationDFS(char* str, int idx, string& res)
 {
 	if (res.size() > 0)
@@ -61,15 +110,82 @@ void Combination(char* str)
 	if (str == NULL || strlen(str) == 0)
 		return;
 
+	sort(str, str + strlen(str));
 	string res;
 	CombinationDFS(str, 0, res);
 }
 
 
+//方法二
+//子串f(i-1)的所有组合已知，则f(i)的所有组合实际上就是f(i-1)的所有组合，再加上f(i-1) + str[i]（把新增的字符加到之前的组合上）
+//用递归实现也可以
+void CombinationDP(const char* str)
+{
+	if (str == NULL || strlen(str) == 0)
+		return;
+
+	vector<string> res;
+
+	int len = strlen(str);
+
+	for (int i = 0; i < len; ++i)
+	{
+		vector<string> tmp;
+
+		if (i == 0 || str[i] != str[i - 1])
+			tmp.push_back(string("") + str[i]);
+
+		for (int j = 0; j < res.size(); ++j)
+		{
+			tmp.push_back(res[j] + str[i]);
+		}
+
+		for (int k = 0; k < tmp.size(); ++k)
+			res.push_back(tmp[k]);
+	}
+
+	for (int i = 0; i < res.size(); ++i)
+	{
+		cout << res[i] << endl;
+	}
+	return;
+}
+
+//基于位操作的方法，不用递归，不用开辟数组。对于某个元素，有选取和不选取两种状态，因此子集和总数是2^n。
+//也就是说，n个元素的集合，对应2^n种子集，每种子集实质上是所有元素的状态集合（二值）。
+//这不就是n位二进制数所对应的所有数吗？Got it! 假设元素小于32，用一个unsign int 就可以了。
+//如果集合有重复，会包含重复结果，可以用set去重
+
+void CombinationBit(char* str)
+{
+	if (str == NULL || strlen(str) == 0)
+	{
+		return;
+	}
+	int len = strlen(str);
+
+	unsigned int i = 1; //对应具体的组合情况
+	unsigned int end_flag = 1 << len;
+	for (; i < end_flag; i++)
+	{
+		int mask = 1;
+		int n = len;
+		while (n--)
+		{
+			if (i & mask)
+			{
+				cout << str[n] << " "; 
+			}
+			mask <<= 1;
+		}
+		cout << endl;
+	}
+}
+
 
 int main()
 {
-	char str[] = {"aa"};
+	char str[] = {"abc"};
 	
 	cout << "original:" << endl;
 	cout << str << endl;
@@ -77,8 +193,17 @@ int main()
 	cout << "permulation:" << endl;
 	Permulation(str);
 
+	cout << "PermulationRecur:" << endl;
+	PermulationRecur(str);
+
 	cout << "combination:" << endl;
 	Combination(str);
+
+	cout << "combination DP:" << endl;
+	CombinationDP(str);
+
+	cout << "combination Bit: " << endl;
+	CombinationBit(str);
 
 	int aa;
 	cin >> aa;
--- QuickSort/QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort/QuickSort.cpp
@@ -1,10 +1,11 @@
 // QuickSort.cpp : 定义控制台应用程序的入口点。
-//
 
 #include "stdafx.h"
 #include <iostream>
 using namespace std;
 
+//两种partition方式
+//方式一、跳坑式
 int AdjustArray(int*a, int first, int last)
 {
 	int pivot=a[first];
@@ -32,11 +33,43 @@ int AdjustArray(int*a, int first, int last)
 	return i;
 }
 
+
+
+void swap(int* arr, int l, int r)
+{
+	int tmp = arr[l];
+	arr[l] = arr[r];
+	arr[r] = tmp;
+	return;
+}
+
+//方式二、交换式
+int partition(int* arr, int left, int right)
+{
+	int mid = left + (right - left) / 2;
+	int pivot = arr[mid];
+
+	while (left < right)
+	{
+		while (arr[left] < pivot)
+			left++;
+		while (arr[right] > pivot)
+			right--;
+
+		if (left < right)
+		{
+			swap(arr, left, right);
+		}
+	}
+	return left;
+}
+
 void QuickSort(int* a,int first,int last)
 {
 	if(first<last)
 	{
-		int i=AdjustArray(a,first,last);
+		//int i=AdjustArray(a,first,last);
+		int i = partition(a, first, last);
 		QuickSort(a,first,i-1);
 		QuickSort(a,i+1,last);
 	}
@@ -45,9 +78,10 @@ void QuickSort(int* a,int first,int last)
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	int a[10]={1,3,5,7,9,2,4,6,8,10};
-	QuickSort(a,0,9);
-	for(int i=0;i<10;i++)
+	int a[]={1, 10, 8, 5, 2, 20};
+	int len = sizeof(a) / sizeof(int);
+	QuickSort(a, 0, len - 1);
+	for(int i=0; i < len; i++)
 		cout<<a[i]<<endl;
 	char * str="hello";
 	cout<<sizeof(*str)<<endl;
